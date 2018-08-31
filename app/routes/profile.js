e99794af781b4d1764183a3c53df7634dcae4060
KarlaJu <karla@makingdevs.com>
2018-08-31 15:54:28 -0500

    #1 Ajax Request

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,13 +1,26 @@
 import Route from '@ember/routing/route';
+import ENV from "../config/environment";
 import RSVP from 'rsvp';
 
 export default Route.extend({
 	model(params){
-
+    let teacher= new RSVP.Promise(function (resolve, reject) {
+      $.ajax({
+        type: 'GET',
+        url: ENV.rootURL+'v1/api/teachers',
+        success: function (data) {
+          resolve(data);
+        },
+        error: function (request, textStatus, error) {
+          reject(error);
+        }
+      });
+    });
 	
 
 			return RSVP.hash({
-		    enrollment: params.enrollment
+		    enrollment: params.enrollment,
+        teacher: teacher
 		});
   }
 });
