8babdcc9a2c9a7dc755a082712172cb95bf46b11
KarlaJu <karla@makingdevs.com>
2018-08-31 15:59:09 -0500

    #2 Adding the mock in my api

--- mirage/config.js
+++ mirage/config.js
@@ -1,9 +1,47 @@
 export default function() {
- this.get('users/current', () => {
+ this.get('v1/api/teachers', () => {
     return {
-      username: 'carlogilmar',
-      email: 'carlogilmar12@gmail.com',
-      name: 'Carlo Gilmar'
+      "id": 180,
+      "username": "cf.ruiz",
+      "enrollment": "M00227967",
+      "author": "ia.quiroga",
+      "status": "Contestada",
+      "campusCode": "LEO",
+      "assignedEvaluation": "Evaluación Docencia I DIC 2017",
+      "skillTests": [
+        {
+          "skillTest": "Cultura EBC",
+          "type": "IMPRECISE"
+        },
+        {
+          "skillTest": "Gestión de Clase",
+          "type": "LATENT"
+        },
+        {
+          "skillTest": "Comunicación Intrapersonal",
+          "type": "PRODUCING"
+        },
+        {
+          "skillTest": "Comunicación Interpersonal",
+          "type": "LATENT"
+        },
+        {
+          "skillTest": "Orientación al alumno",
+          "type": "PRODUCING"
+        },
+        {
+          "skillTest": "Liderazgo",
+          "type": "PRODUCING"
+        },
+        {
+          "skillTest": "Vinculación laboral",
+          "type": "LATENT"
+        },
+        {
+          "skillTest": "Gestión tecnológica",
+          "type": "LATENT"
+        }
+      ]
     };
   });
 }
