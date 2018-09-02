58caaa5a311d5fc3707d956efea22d13eff073fd
KarlaJu <karla@makingdevs.com>
2018-09-01 21:51:24 -0500

    #5 Request for graphic-line with teacher data

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -4,6 +4,7 @@ import RSVP from 'rsvp';
 
 export default Route.extend({
 	model(params){
+    
     let teacher= new RSVP.Promise(function (resolve, reject) {
       $.ajax({
         type: 'GET',
@@ -16,11 +17,50 @@ export default Route.extend({
         }
       });
     });
+
+    let competencias = teacher.skillTests;
+    let lista = [1,2,3,4,1,2,3,4];
+
+
+    let chartOptions = {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: teacher.assignedEvaluation
+      },
+      subtitle:{
+        text: teacher.username
+      },
+      xAxis: {
+        categories: competencias
+      },
+      yAxis: {
+        title:{
+          text: "Nivel"
+        },
+        categories: ['','1 Latente', '2 Impreciso', '3 Gestándose', '4 Produciendo']
+      },
+
+      plotOptions: {
+        line: {
+          dataLabels: {
+            enabled: true
+          },
+          enableMouseTracking: true
+        }
+      },
+      series: [{
+        name: 'Usuario Evaluado',
+        data: lista
+      }]
+    };
 	
 
 			return RSVP.hash({
 		    enrollment: params.enrollment,
-        teacher: teacher
+        teacher: teacher,
+        options: chartOptions
 		});
   }
 });
