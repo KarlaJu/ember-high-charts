65540f87e333d50c6a06f71def4b8c68bbdd9c83
KarlaJu <karla@makingdevs.com>
2018-09-03 16:50:39 -0500

    #6 Char form in profile-chart component

--- app/components/profile-chart.js
+++ app/components/profile-chart.js
@@ -3,10 +3,60 @@ import Component from '@ember/component';
 export default Component.extend({
 
 
-	teacher: Ember.computed('', function(){
+	chartModel: Ember.computed('', function(){
 		let current_teacher = this.get('teacher');
-		return current_teacher;
-	})
 
+		// Call a function
+		let title = this.getTitle();
+
+		let getResults = this.getResults( current_teacher.skillTests );
+
+    let chartModel = {
+      chart: {
+        type: 'line',
+      },
+      title: {
+        text: title
+      },
+      subtitle:{
+        text: "Subtitulo"
+      },
+      xAxis: {
+        categories: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+      },
+      yAxis: {
+        title:{
+          text: "Nivel"
+        },
+        categories: ['','1 Latente', '2 Impreciso', '3 Gestándose', '4 Produciendo']
+      },
+
+      plotOptions: {
+        line: {
+          dataLabels: {
+            enabled: true
+          },
+          enableMouseTracking: true
+        }
+      },
+      series: [{
+        name: 'Usuario Evaluado',
+        data: [1,2,3,4,1,2,3,4]
+      }]
+    };
+
+
+		return chartModel;
+	}),
+
+	getTitle: function(){
+		return "Que me ves?";
+	},
+
+	getResults: function(teacher){
+		let map = {"imprecisa":1, "latente":2, "gestandose":3, "produciendo":4 }
+		list = map[teacher.skillTests.type]
+		return list
+	}
 
 });
