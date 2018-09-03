d9a8b09c756eeab44cf11530b7e76cfd65148969
KarlaJu <karla@makingdevs.com>
2018-09-03 16:08:26 -0500

    #6 Remove innecesary fucking data from my route and implement component

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -3,11 +3,6 @@ import ENV from "../config/environment";
 import RSVP from 'rsvp';
 
 export default Route.extend({
-  beforeModel(/* transition */) {
-    //this.transitionTo('posts'); // Implicitly aborts the on-going transition.
-  
-    console.log("Before model here!!!");
-  },
 
 	model(params){
     
@@ -25,98 +20,9 @@ export default Route.extend({
       });
     }); 
 
-    let teacherExample = {
-      "id": 180,
-      "username": "cf.ruiz",
-      "enrollment": "M00227967",
-      "author": "ia.quiroga",
-      "status": "Contestada",
-      "campusCode": "LEO",
-      "assignedEvaluation": "Evaluación Docencia I DIC 2017",
-      "skillTests": [
-        {
-          "skillTest": "Cultura EBC",
-          "type": "Imprecisa"
-        },
-        {
-          "skillTest": "Gestión de Clase",
-          "type": "Latente"
-        },
-        {
-          "skillTest": "Comunicación Intrapersonal",
-          "type": "Produciendo"
-        },
-        {
-          "skillTest": "Comunicación Interpersonal",
-          "type": "Latente"
-        },
-        {
-          "skillTest": "Orientación al alumno",
-          "type": "Produciendo"
-        },
-        {
-          "skillTest": "Liderazgo",
-          "type": "Produciendo"
-        },
-        {
-          "skillTest": "Vinculación laboral",
-          "type": "Latente"
-        },
-        {
-          "skillTest": "Gestión tecnológica",
-          "type": "Latente"
-        }
-      ]
-    };
-
-
-
-    let competencias = teacherExample.skillTests.skillTest;
-    let lista = [1,2,3,4,1,2,3,4];
-    
-    console.log(teacher.assignedEvaluation);
-    console.log(teacher.username);
-
-
-    let chartOptions = {
-      chart: {
-        type: 'line',
-      },
-      title: {
-        text: teacherExample.assignedEvaluation
-      },
-      subtitle:{
-        text: teacherExample.username
-      },
-      xAxis: {
-        categories: competencias
-      },
-      yAxis: {
-        title:{
-          text: "Nivel"
-        },
-        categories: ['','1 Latente', '2 Impreciso', '3 Gestándose', '4 Produciendo']
-      },
-
-      plotOptions: {
-        line: {
-          dataLabels: {
-            enabled: true
-          },
-          enableMouseTracking: true
-        }
-      },
-      series: [{
-        name: 'Usuario Evaluado',
-        data: lista
-      }]
-    };
-	
-
 			return RSVP.hash({
-		    enrollment: params.enrollment,
-        teacher: teacher,
-        options: chartOptions
-		});
+		    teacher: teacher		
+      });
+
   }
 });
