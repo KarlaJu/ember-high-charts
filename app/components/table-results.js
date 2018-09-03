b4ea46d5f48f05dabe97ad5f7c14aceb57974e2e
KarlaJu <karla@makingdevs.com>
2018-09-03 16:22:38 -0500

    #6 Show results

--- app/components/table-results.js
+++ app/components/table-results.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+
+	teacher: Ember.computed('', function(){
+		let teacher = this.get("teacher");
+		return teacher;
+	})
+
 });
