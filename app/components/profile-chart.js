ffa727b490312cf53596a03d062db631b3f294da
KarlaJu <karla@makingdevs.com>
2018-09-03 16:13:52 -0500

    #6 Get the teacher from route and display the username in the component

--- app/components/profile-chart.js
+++ app/components/profile-chart.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+
+	teacher: Ember.computed('', function(){
+		let current_teacher = this.get('teacher');
+		return current_teacher;
+	})
+
+
 });
